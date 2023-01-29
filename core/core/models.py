47d1a10ce4d094774963a37a5dff68aeb030ae4b
Uros Pocek <uros.pocek@gmail.com>
2023-01-29 22:10:09 +0100

    #21 Changes to model structure to reflect search implementation

--- core/core/models.py
+++ core/core/models.py
@@ -16,12 +16,6 @@ class Graph(models.Model):
     def get_all_nodes(self):
         return self.node_set.all()
 
-    def fileter(self):
-        pass
-
-    def search(self):
-        pass
-
     def __str__(self):
         output = "{name: " + self.name + ",\nnodes: ["
         for node in self.get_all_nodes():
@@ -42,7 +36,7 @@ class Node(models.Model):
     def delete_neighbour(self, node):
         self.neighbours.remove(node)
         self.save()
-
+        
     def delete_neighbour_by_id(self, id):
         try:
             node_to_delete = self.neighbours.get(pk=id)
@@ -53,6 +47,12 @@ class Node(models.Model):
 
     def get_all_neighbours(self):
         return self.neighbours.all()
+    
+    def get_all_neighbour_ids(self):
+        ids = []
+        for n in self.neighbours.all():
+            ids.append(n.id)
+        return ids
 
     def get_neighbour_count(self):
         return self.neighbours.count()
@@ -67,6 +67,9 @@ class Node(models.Model):
 
     def get_atribute_value(self, key):
         return self.atributes.get(key)
+    
+    def get_all_atributes(self):
+        return self.atributes.keys()
 
     def __str__(self):
         return "{id: " + str(self.id) + ", atributes: " + str(self.atributes) + ", neighbours_count: " + str(self.get_neighbour_count()) + "}"
